6dfa8ebe347f804091f9f9b34dd708c20cb02df2
GET Academy <terje@getacademy.no>
2022-10-26 11:26:54 +0200

    Update Program.cs

--- NewsletterSubscription/Program.cs
+++ NewsletterSubscription/Program.cs
@@ -1,10 +1,33 @@
-﻿// Demo code:
-
-
-using NewsletterSubscription.Implementations;using NewsletterSubscription.Model;
+﻿using NewsletterSubscription;
+using NewsletterSubscription.Implementations;
+using NewsletterSubscription.Model;
 
 var emailService = new DummyEmailService();
-var subscriptionRepo= new SubscriptionFileRepository();
+var subscriptionRepo = new SubscriptionFileRepository();
+var subscriptionService = new SubscriptionService(emailService, subscriptionRepo);
+
+while (true)
+{
+    Console.Write("Meny:\n1: Melde på nyhetsbrev\n2: Verifisere\n");
+    var choice = Console.ReadLine();
+    if (choice == "1")
+    {
+        Console.Write("Skriv inn epostadresse: ");
+        var emailAddress = Console.ReadLine();
+        subscriptionService.Subscribe(emailAddress);
+    }
+    else if (choice == "2")
+    {
+        Console.Write("Skriv inn epostadresse: ");
+        var emailAddress = Console.ReadLine();
+        Console.Write("Skriv inn verifikasjonskode: ");
+        var code = Console.ReadLine();
+        subscriptionService.Verify(emailAddress, new Guid(code));
+    }
+}
+
+
+// Demo code:
 
 var email = new Email(
     "per@getacademy.no",
