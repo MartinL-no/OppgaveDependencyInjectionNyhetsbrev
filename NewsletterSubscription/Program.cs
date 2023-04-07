95801a632e08529d9405d7e3d0d2ec4a7bafa4bd
MartinL-no <martin.lorimer@icloud.com>
2023-04-07 18:33:44 +0200

    first part of exercise completed

--- NewsletterSubscription/Program.cs
+++ NewsletterSubscription/Program.cs
@@ -41,3 +41,31 @@ subscriptionRepo.Save(subscription);
 
 subscription = subscriptionRepo.Load("terje@getacademy.no");
 Console.WriteLine(subscription);
+
+/*
+    Start with this code:  github.com/GetAcademy/NewsletterSubscription
+
+    Things are pretty much set up - and what is missing is to implement the methods Subscribe and
+    Verify in SubscriptionService. In Program you will find both finished code with user interface
+    and example code on how to use DummyEmailService and SubscriptionFileRepository.
+
+    The logic is as follows.
+
+    Subscribe 
+
+    Create an object of class Subscription with the email address of the user
+    Save this Subscription using subscriptionRepository.Save
+    Send an email to the user with the verification code using emailService
+
+    Verify
+
+    Read Subscription from file using subscriptionRepository.Load and the email address of the user.
+    Check if the code is correct. If it is
+    set IsVerified to true and save Subscription with subscriptionRepository.Save
+    send an email to the user that the subscription to the newsletter has started
+
+    Extra task
+
+    Do more error checking, so that you are notified if you subscribe to an email address that already exists
+    Learn about interface mocking and check if your code works using unit tests and mocking
+ */
--- NewsletterSubscription/SubscriptionService.cs
+++ NewsletterSubscription/SubscriptionService.cs
@@ -1,4 +1,5 @@
 ﻿using NewsletterSubscription.Interfaces;
+using NewsletterSubscription.Model;
 
 namespace NewsletterSubscription
 {
@@ -17,12 +18,35 @@ namespace NewsletterSubscription
 
         public void Subscribe(string emailAddress)
         {
+            var name = emailAddress.Split('@')[0];
+            var nameUpperCaseFirstLetter = char.ToUpper(name[0]) + name.Substring(1);
+            var subscription = new Subscription(nameUpperCaseFirstLetter, emailAddress, "1894");
+            _subscriptionRepository.Save(subscription);
 
+            var email = new Email(
+                subscription.Email,
+                "info@getacademy.no",
+                "Get Academy Verification Code",
+                $"Hei {subscription.Name} your verification code is {subscription.VerificationCode}");
+
+            _emailService.Send(email);
         }
 
         public void Verify(string emailAddress, string verificationCode)
         {
+            var subscription = _subscriptionRepository.Load(emailAddress);
 
+            if (subscription.VerificationCode == verificationCode)
+            {
+                subscription.IsVerified = true;
+                _subscriptionRepository.Save(subscription);
+                var email = new Email(
+                    subscription.Email,
+                    "info@getacademy.no",
+                    "Get Academy Subscription",
+                    $"Hei {subscription.Name} your subscription to Get Academy has started");
+                _emailService.Send(email);
+            }
         }
     }
 }
